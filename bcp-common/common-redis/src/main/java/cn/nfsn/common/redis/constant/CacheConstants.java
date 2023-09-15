11a1d71bcf47d2a43ec513e4dd37a86e338a4a9a
gaojianjie <a13302361327@163>
2023-09-15 16:41:57 +0800

    feat:新增单点登录和单点下线、注册、手机号和邮箱号短信发送功能

--- bcp-common/common-redis/src/main/java/cn/nfsn/common/redis/constant/CacheConstants.java
+++ bcp-common/common-redis/src/main/java/cn/nfsn/common/redis/constant/CacheConstants.java
@@ -15,39 +15,4 @@ public class CacheConstants {
      * 冒号字符，常用于键的构造
      */
     public static final String REQ_IDEMPOTENCY = "ReqIdempotency:";
-
-    /**
-     * 默认等待时长 1 毫秒
-     */
-    public static final long DEF_WAIT_TIME = 1L;
-
-    /**
-     * 默认过期时长 10 秒
-     */
-    public static final long DEF_EXPIRE_TIME = 1000 * 10L;
-
-    /**
-     * 默认重试次数
-     */
-    public static final Integer NO_TRY_COUNT = 0;
-
-    /**
-     * 默认重试休眠时长
-     */
-    public static final Long NO_TRY_SLEEP_TIME = 0L;
-
-    /**
-     * 线程池大小
-     */
-    public static final int THREAD_POOL_SIZE = 10;
-
-    /**
-     * 缓存空值的存活时间，单位为分钟
-     */
-    public static final Long CACHE_NULL_TTL = 2L;
-
-    /**
-     * 锁前缀
-     */
-    public static final String LOCK_KEY = "lock:";
 }
