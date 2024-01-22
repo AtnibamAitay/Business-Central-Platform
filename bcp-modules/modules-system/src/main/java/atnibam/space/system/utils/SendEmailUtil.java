0d5c67f301877e5e4786202322a1093a069a15af
AtnibamAitay <atnibamaitay@foxmail.com>
2024-01-22 22:08:46 +0800

    Refactor(SendEmailUtil): 优化 SendEmailUtil 类结构与内容生成方法 - 重新组织类中属性定义顺序，将静态变量放置在前。 - 提供了更清晰的方法注释以阐述功能和参数用途。

--- bcp-modules/modules-system/src/main/java/atnibam/space/system/utils/SendEmailUtil.java
+++ bcp-modules/modules-system/src/main/java/atnibam/space/system/utils/SendEmailUtil.java
@@ -10,54 +10,59 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class SendEmailUtil {
-    /**
-     * 发送验证码
-     *
-     * @param email  接收邮箱
-     * @param code   验证码
-     * @return void
-     */
-    //SpringBoot 提供了应该发邮件的简单抽象，
-    // 使用的是下面这个接口，这里直接注入即可使用
-    @Autowired
-    private JavaMailSender javaMailSender;
 
+    public static final String SUFFIX = "，为了您的账号安全，请勿外泄，该验证码5分钟内有效";
     //根据配置文件中自己的QQ邮箱
     private static final String FROM = "1151214239@qq.com";
 
     public String loginPrefix = "验证码为:";
+    @Autowired
+    private JavaMailSender javaMailSender;
 
-    public static final String SUFFIX = "，为了您的账号安全，请勿外泄，该验证码5分钟内有效";
-
-    /*
-    @Param to 收件人
-    @Param subject 主题
-    @Param content
+    /**
+     * 根据给定的前缀、内容和主题生成邮件内容
+     *
+     * @param prefix  验证码前缀
+     * @param content 邮件内容
+     * @param subject 邮件主题
+     * @return 邮件内容
      */
-
-    public String getContent(String content,String subject) {
-        return getContent(loginPrefix, content,subject);
+    public static String getContent(String prefix, String content, String subject) {
+        // 生成邮件内容
+        return subject + prefix + content + SUFFIX;
     }
 
-    public static String getContent(String prefix, String content,String subject) {
-        return subject+prefix + content + SUFFIX;
+    /**
+     * 根据给定的内容和主题生成邮件内容
+     *
+     * @param content 邮件内容
+     * @param subject 邮件主题
+     * @return 邮件内容
+     */
+    public String getContent(String content, String subject) {
+        // 调用静态方法getContent生成邮件内容
+        return getContent(loginPrefix, content, subject);
     }
 
-    //发送普通邮件
+    /**
+     * 发送简单邮件
+     *
+     * @param to      接收者邮箱
+     * @param subject 邮件主题
+     * @param code    邮件内容中的验证码
+     */
     public void sendSimpleMail(String to, String subject, String code) {
         //创建SimpleMailMessage对象
         SimpleMailMessage message = new SimpleMailMessage();
-        //邮件发送
+        //设置邮件发送者
         message.setFrom(FROM);
-        //邮件接收人,可以同时发给很多人
-        //message.setTo(string1,string2,string3,string4,);
+        //设置邮件接收人
         message.setTo(to);
-        //邮件主体
+        //设置邮件主题
         message.setSubject(subject);
-        //邮件内容
-        message.setText(getContent(code,subject));
+        //设置邮件内容
+        message.setText(getContent(code, subject));
         //通过JavaMailSender类把邮件发送出去
         javaMailSender.send(message);
     }
-
 }
