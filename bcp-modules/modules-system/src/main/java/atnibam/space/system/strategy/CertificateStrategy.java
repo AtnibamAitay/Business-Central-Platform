65ec87a99151f5b9f0aff1d760257a3d010b2bc7
AtnibamAitay <atnibamaitay@foxmail.com>
2024-01-22 22:21:43 +0800

    Modify(CertificateStrategy): 更新接口注释及方法顺序 - 为getPrefixByType方法添加详细JavaDoc注释，明确其功能为根据类型获取Redis key前缀 - 调整sendCodeHandler和saveVerificationCodeToRedis方法的顺序，提高代码逻辑阅读性 - 修改类注释，明确CertificateStrategy接口为证书策略接口
    本次提交对CertificateStrategy接口进行了文档更新，并调整了方法顺序以提升代码可读性和维护性。

--- bcp-modules/modules-system/src/main/java/atnibam/space/system/strategy/CertificateStrategy.java
+++ bcp-modules/modules-system/src/main/java/atnibam/space/system/strategy/CertificateStrategy.java
@@ -7,22 +7,41 @@ import atnibam.space.common.core.exception.SystemServiceException;
 import atnibam.space.common.redis.constant.CacheConstants;
 
 /**
- * @Author: gaojianjie
- * @date 2023/9/11 08:23
+ * 证书策略接口
  */
 public interface CertificateStrategy {
-    void sendCodeHandler(String account,String code,String subject);
+    /**
+     * 根据类型获取Redis key前缀
+     *
+     * @param integer 类型
+     * @return Redis key前缀
+     * @throws SystemServiceException 如果类型无效，则抛出异常
+     */
+    static String getPrefixByType(Integer integer) {
+        switch (integer) {
+            case UserConstants.PREFIX_TYPE_1:
+                return CacheConstants.LOGIN_CODE_KEY;
+            case UserConstants.PREFIX_TYPE_2:
+                return CacheConstants.BINDING_CODE_KEY;
+            default:
+                throw new SystemServiceException(ResultCode.PARAM_IS_INVALID);
+        }
+    }
 
+    /**
+     * 发送验证码处理方法
+     *
+     * @param account 账号
+     * @param code    码
+     * @param subject 主题
+     */
+    void sendCodeHandler(String account, String code, String subject);
+
+    /**
+     * 将验证码保存到Redis
+     *
+     * @param verifyCodeDTO 验证码信息
+     * @param code          码
+     */
     void saveVerificationCodeToRedis(VerifyCodeDTO verifyCodeDTO, String code);
-
-     static String getPrefixByType(Integer integer) {
-         switch (integer) {
-             case UserConstants.PREFIX_TYPE_1:
-                 return CacheConstants.LOGIN_CODE_KEY;
-             case UserConstants.PREFIX_TYPE_2:
-                 return CacheConstants.BINDING_CODE_KEY;
-             default:
-                 throw new SystemServiceException(ResultCode.PARAM_IS_INVALID);
-         }
-     }
 }
