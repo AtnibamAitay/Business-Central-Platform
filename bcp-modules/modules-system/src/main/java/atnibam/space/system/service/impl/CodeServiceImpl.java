4bb053d51c6e3a15751fa013fb95eca2741bd233
AtnibamAitay <atnibamaitay@foxmail.com>
2024-01-22 22:19:46 +0800

    docs(CodeService): 重构CodeServiceImpl类注释及方法描述 - 更新类注释为 `CodeService` 实现类并提供更清晰的职责说明 - 修改 `sendCode` 方法注释，细化参数含义与功能描述 - 简化代码格式以提升可读性，无功能性变更

--- bcp-modules/modules-system/src/main/java/atnibam/space/system/service/impl/CodeServiceImpl.java
+++ bcp-modules/modules-system/src/main/java/atnibam/space/system/service/impl/CodeServiceImpl.java
@@ -1,35 +1,39 @@
 package atnibam.space.system.service.impl;
 
 import atnibam.space.common.core.constant.Constants;
-import atnibam.space.common.core.enums.CertificateMethodEnum;
-import cn.hutool.core.util.RandomUtil;
 import atnibam.space.common.core.domain.dto.VerifyCodeDTO;
+import atnibam.space.common.core.enums.CertificateMethodEnum;
 import atnibam.space.system.factory.CertificateStrategyFactory;
 import atnibam.space.system.service.CodeService;
 import atnibam.space.system.strategy.CertificateStrategy;
+import cn.hutool.core.util.RandomUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * @Author: gaojianjie
- * @Description 短信服务
- * @date 2023/9/12 19:24
+ * CodeService实现类
  */
 @Service
 public class CodeServiceImpl implements CodeService {
     @Autowired
     private CertificateStrategyFactory certificateStrategyFactory;
+
+    /**
+     * 发送验证码
+     *
+     * @param verifyCodeDTO 验证码信息
+     */
     @Override
     public void sendCode(VerifyCodeDTO verifyCodeDTO) {
-        //生成验证码
+        // 生成验证码
         String code = RandomUtil.randomNumbers(Constants.RANDOM_LENGTH);
-        //提取发送目标
+        // 发送目标
         String account = verifyCodeDTO.getAccount();
-        //提取主题信息
+        // 主题（手机号登录则为短信主题，邮箱登录则为邮件标题）
         String subject = verifyCodeDTO.getSubject();
-        //发送验证码
+        // 发送验证码
         CertificateStrategy strategy = certificateStrategyFactory.getStrategy(CertificateMethodEnum.fromCode(verifyCodeDTO.getIdentity()));
-        strategy.sendCodeHandler(account,code,subject);
-        strategy.saveVerificationCodeToRedis(verifyCodeDTO,code);
+        strategy.sendCodeHandler(account, code, subject);
+        strategy.saveVerificationCodeToRedis(verifyCodeDTO, code);
     }
 }
