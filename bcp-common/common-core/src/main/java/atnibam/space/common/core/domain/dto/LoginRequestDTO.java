af9b5aa31bd477ae4b47fc76d11a0ff890b86871
AtnibamAitay <atnibamaitay@foxmail.com>
2024-01-22 21:54:55 +0800

    Refactor(LoginRequestDTO): 优化登录请求类结构及注释 - 使用Lombok的@Data和@NoArgsConstructor注解简化getter/setter方法和无参构造函数 - 修改`certificate`字段的注释，明确支持邮箱或手机号作为登录凭证 - 将`loginMethod`字段的注释内容内联至`@ApiModelProperty`中 - 删除了冗余的空行和空格，改善代码格式与可读性

--- bcp-common/common-core/src/main/java/atnibam/space/common/core/domain/dto/LoginRequestDTO.java
+++ bcp-common/common-core/src/main/java/atnibam/space/common/core/domain/dto/LoginRequestDTO.java
@@ -2,25 +2,29 @@ package atnibam.space.common.core.domain.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @ApiModel("登陆注册请求类")
+@Data
+@NoArgsConstructor
 public class LoginRequestDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /**
-     * 凭证-手机号登录凭证为手机号码，微信和QQ则为对应的凭据
+     * 凭证 手机号登录凭证为手机号码，邮箱登录凭证为邮箱
      */
-    @ApiModelProperty("邮箱或手机账号")
+    @ApiModelProperty("邮箱或手机号")
     @NotNull(message = "账号不能为空")
     @NotBlank(message = "账号不能为空")
     private String certificate;
 
     /**
-     * 验证码-
+     * 验证码
      */
     @ApiModelProperty("验证码")
     @NotNull(message = "验证码不能为空")
@@ -28,63 +32,19 @@ public class LoginRequestDTO implements Serializable {
     private String verifyCode;
 
     /**
-     * 登录方式 1-手机号+验证码 2-邮箱+验证码
+     * 登录方式
+     * 1-手机号+验证码
+     * 2-邮箱+验证码
      */
     @ApiModelProperty("登录方式 1-手机号+验证码 2-邮箱+验证码")
     @NotNull(message = "登录方式不能为空")
-//    @ApiModelProperty("1-手机号+验证码 2-邮箱+验证码 ")
     private Integer loginMethod = 0;
 
     /**
-     * 应用码-
+     * 应用码
      */
     @ApiModelProperty("应用ID 1 STEAM课堂 2 北极星宠 3万象课堂")
     @NotNull(message = "应用码不能为空")
     @NotBlank(message = "应用码不能为空")
     private String appCode;
-
-    public String getCertificate() {
-        return certificate;
-    }
-
-    public void setCertificate(String certificate) {
-        this.certificate = certificate;
-    }
-
-    public String getVerifyCode() {
-        return verifyCode;
-    }
-
-    public void setVerifyCode(String verifyCode) {
-        this.verifyCode = verifyCode;
-    }
-
-    public Integer getLoginMethod() {
-        return loginMethod;
-    }
-
-    public void setLoginMethod(Integer loginMethod) {
-        this.loginMethod = loginMethod;
-    }
-
-    public LoginRequestDTO() {
-    }
-
-    public String getAppCode() {
-        return appCode;
-    }
-
-    public void setAppCode(String appCode) {
-        this.appCode = appCode;
-    }
-
-    @Override
-    public String toString() {
-        return "LoginRequestDTO{" +
-                "certificate='" + certificate + '\'' +
-                ", verifyCode='" + verifyCode + '\'' +
-                ", loginMethod=" + loginMethod +
-                ", appCode='" + appCode + '\'' +
-                '}';
-    }
 }
