189a681c9424dd048c352b68cb2725e14321d915
AtnibamAitay <atnibamaitay@foxmail.com>
2024-01-22 22:47:47 +0800

    Modify(EmailCertificateStrategy): 优化代码结构及注释 - 对`EmailCertificateStrategy`类进行了重构，增加了详细的JavaDoc注释以提高代码可读性。 - 方法`sendCodeHandler`和`saveVerificationCodeToRedis`添加了注释说明其功能和参数用途。 - `checkCertificate`方法现在具有明确的JavaDoc描述，并在方法体内添加了异常抛出原因的注释。

--- bcp-modules/modules-system/src/main/java/atnibam/space/system/strategy/impl/EmailCertificateStrategy.java
+++ bcp-modules/modules-system/src/main/java/atnibam/space/system/strategy/impl/EmailCertificateStrategy.java
@@ -6,16 +6,15 @@ import atnibam.space.common.core.exception.UserOperateException;
 import atnibam.space.common.core.utils.ValidatorUtil;
 import atnibam.space.common.redis.constant.CacheConstants;
 import atnibam.space.common.redis.service.RedisCache;
-import atnibam.space.system.utils.SendEmailUtil;
 import atnibam.space.system.strategy.CertificateStrategy;
+import atnibam.space.system.utils.SendEmailUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * @Author: gaojianjie
- * @date 2023/9/11 08:26
+ * 邮箱证书策略类
  */
 @Component
 public class EmailCertificateStrategy implements CertificateStrategy {
@@ -24,22 +23,46 @@ public class EmailCertificateStrategy implements CertificateStrategy {
     @Autowired
     private SendEmailUtil sendEmailUtil;
 
+    /**
+     * 发送验证码处理方法
+     *
+     * @param account 邮箱账号
+     * @param code    验证码
+     * @param subject 主题
+     */
     @Override
-    public void sendCodeHandler(String account, String code,String subject) {
+    public void sendCodeHandler(String account, String code, String subject) {
+        // 检查证书
         checkCertificate(account);
-        sendEmailUtil.sendSimpleMail(account,subject,code);
+        // 发送邮件
+        sendEmailUtil.sendSimpleMail(account, subject, code);
     }
 
+    /**
+     * 将验证码保存到Redis中
+     *
+     * @param verifyCodeDTO 验证码信息
+     * @param code          验证码
+     */
     @Override
     public void saveVerificationCodeToRedis(VerifyCodeDTO verifyCodeDTO, String code) {
+        // 获取前缀
         String prefix = CertificateStrategy.getPrefixByType(verifyCodeDTO.getFunctionType());
+        // 检查证书
         checkCertificate(verifyCodeDTO.getAccount());
-        redisCache.setCacheObject(prefix+ CacheConstants.EMAIL_KEY+verifyCodeDTO.getAccount(),code,CacheConstants.MESSAGE_CODE_TIME_OUT, TimeUnit.MINUTES);
+        // 保存验证码到Redis缓存中
+        redisCache.setCacheObject(prefix + CacheConstants.EMAIL_KEY + verifyCodeDTO.getAccount(), code, CacheConstants.MESSAGE_CODE_TIME_OUT, TimeUnit.MINUTES);
     }
-    private void checkCertificate(String certificate){
-        if(!ValidatorUtil.isEmail(certificate)){
+
+    /**
+     * 检查证书方法，判断是否为合法邮箱格式
+     *
+     * @param certificate 证书（邮箱）
+     * @throws UserOperateException 若不是合法邮箱格式，则抛出用户操作异常
+     */
+    private void checkCertificate(String certificate) {
+        if (!ValidatorUtil.isEmail(certificate)) {
             throw new UserOperateException(ResultCode.EMAIL_NUM_NON_COMPLIANCE);
         }
     }
-
 }
