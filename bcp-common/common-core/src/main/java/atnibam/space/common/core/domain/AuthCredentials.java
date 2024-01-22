18d4c23a394d437f72887bd918f3fd7ce6b61d8f
AtnibamAitay <atnibamaitay@foxmail.com>
2024-01-22 21:06:48 +0800

    modify：(AuthCredentials): 优化实体类结构及注解 - 使用Lombok的@Data注解自动添加getter/setter方法 - 简化字段注释，对credentialsId字段添加更清晰的描述 - 移除无用的空行和冗余的方法

--- bcp-common/common-core/src/main/java/atnibam/space/common/core/domain/AuthCredentials.java
+++ bcp-common/common-core/src/main/java/atnibam/space/common/core/domain/AuthCredentials.java
@@ -4,93 +4,32 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
 import java.io.Serializable;
 
-/**
- *
- * @TableName auth_credentials
- */
-@TableName(value ="auth_credentials")
+@Data
+@TableName(value = "auth_credentials")
 public class AuthCredentials implements Serializable {
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
     /**
-     *
+     * 用户凭证Id
      */
     @TableId(type = IdType.AUTO)
     private Integer credentialsId;
-
     /**
      * 身份证
      */
     private String idCardNumber;
-
     /**
      * 手机号
      */
     private String phoneNumber;
-
     /**
      * 邮箱
      */
     private String email;
-
-    @TableField(exist = false)
-    private static final long serialVersionUID = 1L;
-
-    /**
-     *
-     */
-    public Integer getCredentialsId() {
-        return credentialsId;
-    }
-
-    /**
-     *
-     */
-    public void setCredentialsId(Integer credentialsId) {
-        this.credentialsId = credentialsId;
-    }
-
-    /**
-     * 身份证
-     */
-    public String getIdCardNumber() {
-        return idCardNumber;
-    }
-
-    /**
-     * 身份证
-     */
-    public void setIdCardNumber(String idCardNumber) {
-        this.idCardNumber = idCardNumber;
-    }
-
-    /**
-     * 手机号
-     */
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    /**
-     * 手机号
-     */
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    /**
-     * 邮箱
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * 邮箱
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     @Override
     public boolean equals(Object that) {
