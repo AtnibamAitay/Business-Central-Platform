9228532d6433ca3bcd051a6371483a1310b19783
AtnibamAitay <atnibamaitay@foxmail.com>
2024-01-24 17:17:10 +0800

    docs(CertificateMethodEnum): 枚举注释及文档改进
    - 更新用户认证方法枚举类 CertificateMethodEnum 的注释，使其更加详细且规范
    - 修改登录方式描述为认证方法，并完善各枚举值的注释信息
    - 优化 fromCode 方法的文档注释，明确方法功能和异常抛出条件

--- bcp-common/common-core/src/main/java/atnibam/space/common/core/enums/CertificateMethodEnum.java
+++ bcp-common/common-core/src/main/java/atnibam/space/common/core/enums/CertificateMethodEnum.java
@@ -1,24 +1,36 @@
 package atnibam.space.common.core.enums;
 
 /**
- * @author gaojianjie
- * @Description 用户登陆方式枚举
+ * 用户认证方法枚举
  */
 public enum CertificateMethodEnum {
-    //通过手机号和验证码登陆
+    /**
+     * 通过手机号和验证码登录
+     */
     PHONE_CODE(1),
-    //通过邮箱和验证码登陆
+    /**
+     * 通过邮箱和验证码登录
+     */
     EMAIL_CODE(2);
 
     private final int code;
+
+    /**
+     * 构造方法
+     *
+     * @param code 认证方法代码
+     */
     CertificateMethodEnum(int code) {
         this.code = code;
     }
 
-    public int getCode() {
-        return code;
-    }
-
+    /**
+     * 根据认证方法代码获取对应的认证方法
+     *
+     * @param code 认证方法代码
+     * @return 对应的认证方法
+     * @throws IllegalArgumentException 如果无效的认证方法代码
+     */
     public static CertificateMethodEnum fromCode(int code) {
         for (CertificateMethodEnum method : CertificateMethodEnum.values()) {
             if (method.getCode() == code) {
@@ -26,5 +38,14 @@ public enum CertificateMethodEnum {
             }
         }
         throw new IllegalArgumentException("Invalid login method code: " + code);
+    }
+
+    /**
+     * 获取认证方法代码
+     *
+     * @return 认证方法代码
+     */
+    public int getCode() {
+        return code;
     }
 }
