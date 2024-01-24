26c7905c1ee9bb993696075b05d07f2132337098
AtnibamAitay <atnibamaitay@foxmail.com>
2024-01-24 19:22:37 +0800

    feat(AuthCredentialsService): 新增邮箱和手机号存在性检查功能
    - AuthCredentialsService 接口中新增 checkEmailExist 和 checkPhoneExist 方法定义，用于检查邮箱和手机号是否已存在于数据库中。
    - AuthCredentialsServiceImpl 类实现了上述接口方法，并通过调用 AuthCredentialsMapper 中的对应查询方法来完成逻辑验证。
    - 当邮箱或手机号不存在时，抛出 UserLoginException 异常。
    
    此次更新增强了对用户登录凭据（邮箱和手机号）校验的功能，以确保数据一致性及安全。

--- bcp-modules/modules-system/src/main/java/atnibam/space/system/service/AuthCredentialsService.java
+++ bcp-modules/modules-system/src/main/java/atnibam/space/system/service/AuthCredentialsService.java
@@ -9,5 +9,17 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @createDate 2024-01-23 17:39:07
  */
 public interface AuthCredentialsService extends IService<AuthCredentials> {
+    /**
+     * 检查邮箱是否存在
+     *
+     * @param email 邮箱
+     */
+    void checkEmailExist(String email);
 
+    /**
+     * 检查手机号是否存在
+     *
+     * @param phoneNumber 手机号
+     */
+    void checkPhoneExist(String phoneNumber);
 }
--- bcp-modules/modules-system/src/main/java/atnibam/space/system/service/impl/AuthCredentialsServiceImpl.java
+++ bcp-modules/modules-system/src/main/java/atnibam/space/system/service/impl/AuthCredentialsServiceImpl.java
@@ -1,10 +1,15 @@
 package atnibam.space.system.service.impl;
 
+import atnibam.space.system.exception.UserLoginException;
 import atnibam.space.system.mapper.AuthCredentialsMapper;
 import atnibam.space.system.model.entity.AuthCredentials;
 import atnibam.space.system.service.AuthCredentialsService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import static atnibam.space.common.core.enums.ResultCode.USER_NOT_EXIST_BY_CODE;
 
 /**
  * @author Atnibam Aitay
@@ -14,5 +19,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthCredentialsServiceImpl extends ServiceImpl<AuthCredentialsMapper, AuthCredentials>
         implements AuthCredentialsService {
+    @Resource
+    private AuthCredentialsMapper authCredentialsMapper;
 
+    /**
+     * 检查邮箱是否存在
+     *
+     * @param email 邮箱
+     */
+    @Override
+    public void checkEmailExist(String email) {
+        if (!authCredentialsMapper.checkEmailExist(email)) {
+            throw new UserLoginException(USER_NOT_EXIST_BY_CODE);
+        }
+    }
+
+    /**
+     * 检查手机号是否存在
+     *
+     * @param phoneNumber 手机号
+     */
+    @Override
+    public void checkPhoneExist(String phoneNumber) {
+        if (authCredentialsMapper.checkPhoneExist(phoneNumber)) {
+            throw new UserLoginException(USER_NOT_EXIST_BY_CODE);
+        }
+    }
 }
