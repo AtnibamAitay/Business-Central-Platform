cb0edfae3f99622523346be57904705ed0ada877
AtnibamAitay <atnibamaitay@foxmail.com>
2024-01-24 18:53:27 +0800

    feat(utils): 新增发送邮件工具类
    - 添加了EmailSenderUtil类，用于发送简单文本邮件。
    - 实现了sendSimpleMail方法，可以设置邮件的发件人、收件人、主题和内容。
    - 使用JavaMailSender进行邮件的实际发送操作。
    
    本次提交添加了邮件发送功能，方便系统通过电子邮件与用户交互及发送通知消息。

---
+++ bcp-modules/modules-system/src/main/java/atnibam/space/system/utils/EmailSenderUtil.java
@@ -0,0 +1,53 @@
+package atnibam.space.system.utils;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import static atnibam.space.system.constant.SystemConstants.EMAIL_FROM;
+
+/**
+ * @ClassName: EmailSenderUtil
+ * @Description: 发送邮件工具类
+ * @Author: AtnibamAitay
+ * @CreateTime: 2024-01-23 19:58
+ **/
+@Component
+public class EmailSenderUtil {
+    @Resource
+    private JavaMailSender javaMailSender;
+
+    /**
+     * 发送邮件
+     *
+     * @param to      收件人
+     * @param title   邮件主题
+     * @param content 邮件内容
+     */
+    public void sendSimpleMail(String to, String title, String content) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        //邮件发送
+        try {
+            //创建SimpleMailMessage对象
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true);
+            //发件人
+            message.setFrom(EMAIL_FROM);
+            //收件人
+            message.setTo(to);
+            //邮件标题
+            message.setSubject(title);
+            //邮件内容
+            message.setText(content, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        //通过JavaMailSender类把邮件发送出去
+        javaMailSender.send(mimeMessage);
+    }
+}
