d8f45c9884c0e589620036c69959848539b3b01e
AtnibamAitay <atnibamaitay@foxmail.com>
2024-01-25 19:41:10 +0800

    feat(bcp-auth): 添加Thymeleaf模板引擎支持和邮件内容构建方法
    - 在`bcp-auth`模块的`pom.xml`中新增了对`spring-boot-starter-thymeleaf`的依赖，引入Thymeleaf模板引擎。
    - 修改了`EmailSendCodeStrategy`类，添加了`TemplateEngine`类型的成员变量，并通过`@Resource`注解进行注入。
    - 新增了`buildEmailContent`方法，用于根据AccountVerificationDTO和验证码动态构建邮件内容，使用Thymeleaf模板引擎处理字符串模板。
    
    此次更新改进了邮件发送策略，使其能够基于Thymeleaf模板生成自定义邮件内容，增强了系统的灵活性和可扩展性。

--- bcp-auth/src/main/java/atnibam/space/auth/strategy/impl/EmailSendCodeStrategy.java
+++ bcp-auth/src/main/java/atnibam/space/auth/strategy/impl/EmailSendCodeStrategy.java
@@ -7,6 +7,9 @@ import atnibam.space.auth.utils.EmailUtil;
 import atnibam.space.common.redis.utils.CacheClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.StringTemplateResolver;
 
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
@@ -23,6 +26,8 @@ public class EmailSendCodeStrategy implements SendCodeStrategy {
     private CacheClient redisCache;
     @Resource
     private EmailUtil emailUtil;
+    @Resource
+    private TemplateEngine templateEngine;
     @Resource
     private AuthCredentialsService authCredentialsService;
 
@@ -41,7 +46,7 @@ public class EmailSendCodeStrategy implements SendCodeStrategy {
         authCredentialsService.checkEmailExist(email);
         // 发送邮件
         // TODO:实现线程池异步发送
-        emailUtil.sendSimpleMail(email, accountVerificationDTO.getTitle(), accountVerificationDTO.getContent());
+        emailUtil.sendSimpleMail(email, accountVerificationDTO.getTitle(), buildEmailContent(accountVerificationDTO, code));
     }
 
     /**
@@ -53,5 +58,33 @@ public class EmailSendCodeStrategy implements SendCodeStrategy {
     @Override
     public void saveVerificationCodeToRedis(String email, String code) {
         redisCache.setWithLogicalExpire(LOGIN_EMAIL_CODE_KEY + email, code, CODE_TTL, TimeUnit.MINUTES);
+    }
+
+    /**
+     * 构建邮件内容
+     *
+     * @param accountVerificationDTO 包含邮箱、邮箱内容等信息的数据传输对象
+     * @param code                   验证码
+     * @return 邮件内容
+     */
+    private String buildEmailContent(AccountVerificationDTO accountVerificationDTO, String code) {
+        Context context = new Context();
+        context.setVariable("email", accountVerificationDTO.getAccountNumber());
+        context.setVariable("verifyCode", code);
+        context.setVariable("ttl", CODE_TTL);
+
+        // 创建并配置StringTemplateResolver
+        StringTemplateResolver stringTemplateResolver = new StringTemplateResolver();
+        // 设置模板模式为HTML
+        stringTemplateResolver.setTemplateMode("HTML");
+
+        // 将StringTemplateResolver临时添加到TemplateEngine的模板解析器链中
+        templateEngine.addTemplateResolver(stringTemplateResolver);
+
+        // 使用字符串形式的HTML模板内容
+        String emailTemplateContent = accountVerificationDTO.getContent();
+
+        // 处理字符串模板并公会
+        return templateEngine.process(emailTemplateContent, context);
     }
 }
