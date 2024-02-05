2797a4b6fb46f45434f72e17c95996b83a894819
AtnibamAitay <atnibamaitay@foxmail.com>
2024-02-05 23:56:31 +0800

    refactor(cms, feign): 重构远程评论服务接口及控制器
    - 重命名 `ArticleFeignInterceptor` 为 `CmsFeignInterceptor` 并更新描述
    - 将 `CommentController` 中的 API 路径从 `/api/v1/comments` 修改为 `/api/comments`
    - 更新 `RemoteCommentService` 接口，将其更名为 `RemoteCommentService` 并调整注解为 `@FeignClient(value = "modules-cms", contextId = "comment")`
    - 在 `RemoteCommentService` 中新增了 `getCommentsByObjectId` 方法以支持根据对象ID获取评论树功能
    - 对 `CommentController` 中的 `getComments` 方法进行了名称调整，现在为 `getCommentsByObjectId` 以匹配远程服务接口方法名
    - 维护文档注释，确保与代码更改同步

--- bcp-api/api-cms/src/main/java/space/atnibam/api/cms/RemoteCommentService.java
+++ bcp-api/api-cms/src/main/java/space/atnibam/api/cms/RemoteCommentService.java
@@ -1,27 +1,20 @@
-package space.atnibam.cms.controller;
+package space.atnibam.api.cms;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.*;
-import space.atnibam.cms.service.CommentService;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import space.atnibam.common.core.domain.R;
 
-import javax.annotation.Resource;
-
 /**
- * @ClassName: CommentController
- * @Description: 处理与评论相关动作的控制器。
+ * @InterfaceName: RemoteCommentService
+ * @Description: 远程评论服务
  * @Author: AtnibamAitay
- * @CreateTime: 2023-10-20 13:54
+ * @CreateTime: 2024-02-03 10:53
  **/
-@Api(tags = "CommentAPI")
-@RestController
-@RequestMapping("/api/comments")
-public class CommentController {
-
-    @Resource
-    private CommentService commentService;
+@FeignClient(value = "modules-cms", contextId = "comment")
+public interface RemoteCommentService {
 
     /**
      * 根据对象ID获取评论树
@@ -32,16 +25,12 @@ public class CommentController {
      * @param pageSize 每一页的大小，默认值为10
      * @return 返回构建的评论树
      */
-    @ApiOperation(value = "通过对象ID获取评论树")
-    @GetMapping("/{objectId}")
-    public R getCommentsByObjectId(
+    @GetMapping("/api/comments/{objectId}")
+    R getCommentsByObjectId(
             @ApiParam(value = "需要获取评论的对象的ID。", required = true)
             @PathVariable Integer objectId,
             @ApiParam(value = "需要检索的评论的页数。", defaultValue = "1")
             @RequestParam(defaultValue = "1") Integer pageNum,
             @ApiParam(value = "要检索的评论每一页的大小。", defaultValue = "10")
-            @RequestParam(defaultValue = "10") Integer pageSize) {
-        return commentService.getCommentByObjectId(objectId, pageNum, pageSize);
-    }
-
+            @RequestParam(defaultValue = "10") Integer pageSize);
 }
