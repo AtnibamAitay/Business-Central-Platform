b55e58adb799b96b01f4577927b7f4d0089c50ba
AtnibamAitay <atnibamaitay@foxmail.com>
2024-02-10 13:38:58 +0800

    chore(maven, dependencies): 添加api-ums模块依赖并启用Feign客户端
    - 在pom.xml中新增了对atnibam.space.api-ums模块的依赖项
    - PmsApplication.java中通过@EnableFeignClients注解启用了Feign客户端功能
    
    本次提交更新了项目构建文件，添加了必要的模块依赖关系，并为服务间调用引入了Feign客户端支持。

--- bcp-modules/modules-pms/src/main/java/space/atnibam/PmsApplication.java
+++ bcp-modules/modules-pms/src/main/java/space/atnibam/PmsApplication.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import space.atnibam.common.swagger.annotation.EnableCustomSwagger;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("space.atnibam.**.mapper")
 @EnableCustomSwagger
+@EnableFeignClients
 public class PmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(PmsApplication.class, args);
