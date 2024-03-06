097aac529e45e9e1800995450ffd45bb55455c71
AtnibamAitay <atnibamaitay@foxmail.com>
2024-03-06 11:45:32 +0800

    refactor(RemoteAuthService): 重构RemoteAuthService接口的登录功能
    - 移除发送验证码和单点登陆相关API注解
    - 修改ssoLogin方法返回类型，从SaResult更改为LoginDTO
    - 此次更新针对RemoteAuthService接口进行了内部逻辑调整与优化，改进了服务端单点登录功能的实现方式和响应内容结构。

--- bcp-api/api-auth/src/main/java/space/atnibam/api/auth/RemoteAuthService.java
+++ bcp-api/api-auth/src/main/java/space/atnibam/api/auth/RemoteAuthService.java
@@ -1,12 +1,11 @@
 package space.atnibam.api.auth;
 
-import cn.dev33.satoken.util.SaResult;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import space.atnibam.api.auth.dto.AccountVerificationDTO;
+import space.atnibam.api.auth.dto.LoginDTO;
 import space.atnibam.api.auth.dto.LoginRequestDTO;
 import space.atnibam.common.core.domain.R;
 
@@ -26,7 +25,6 @@ public interface RemoteAuthService {
      * @param accountVerificationDTO 包含账号、验证码类型等信息的数据传输对象
      * @return Result 发送结果，成功发送则返回成功信息，否则返回失败原因
      */
-    @ApiOperation(value = "发送验证码")
     @PostMapping("/api/auth/verification-codes")
     R sendCodeByAccount(@RequestBody @Validated AccountVerificationDTO accountVerificationDTO);
 
@@ -36,7 +34,6 @@ public interface RemoteAuthService {
      * @param loginRequestDTO 登录请求DTO
      * @return 认证结果
      */
-    @ApiOperation("单点登陆")
     @PostMapping("/api/auth/login")
-    SaResult ssoLogin(@Validated @RequestBody LoginRequestDTO loginRequestDTO) throws IOException;
+    LoginDTO ssoLogin(@Validated @RequestBody LoginRequestDTO loginRequestDTO) throws IOException;
 }
